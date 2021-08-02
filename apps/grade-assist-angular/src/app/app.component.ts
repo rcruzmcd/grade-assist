157c608506c9165e3e1a2e46ed2ea7185f96ae86
rcruzmcd <rcruz000@live.com>
2021-08-02 13:00:21 -0400

    nx setup with api, react, and angular backbone

---
+++ apps/grade-assist-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'grade-assist-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'grade-assist-angular';
+}
